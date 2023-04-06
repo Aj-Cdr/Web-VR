76f7280dce4b6bf5ebdfc07fe5a652e3d03de790
Ajay Sathish Preetha <95515701+Aj-Cdr@users.noreply.github.com>
2023-04-06 11:31:30 -0700

    Add files via upload

--- Bowling Game/js/throw.js
+++ Bowling Game/js/throw.js
@@ -19,6 +19,11 @@ AFRAME.registerComponent("bowling-balls", {
                     z: pos.z
                 })
 
+                bowling_ball.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: 0
+                })
+
                 var cam = document.querySelector("#camera").object3D
                 var direction = new THREE.Vector3()
                 cam.getWorldDirection(direction)
@@ -26,10 +31,28 @@ AFRAME.registerComponent("bowling-balls", {
                 bowling_ball.setAttribute("velocity", direction.multiplyScalar(-10));
 
                 var scene = document.querySelector("#scene");
-
                 scene.appendChild(bowling_ball);
+
+                bowling_ball.addEventListener("collide", this.removeBowlingBall())
             }
         })
+    },
+
+    removeBowlingBall: function (e) {
+        var element_thats_getting_hit_pins = e.detail.body.el
+        var element_bowling_ball = e.detail.target.el
+
+        if (element_thats_getting_hit_pins.id.includes("pin")) {
+            var impulse = new CANNON.Vec3(0, 1, -15);
+            var worldPoint = new CANNON.Vec3().copy(element_thats_getting_hit_pins.getAttribute("position"))
+        }
+
+        element_thats_getting_hit_pins.body.applyForce(impulse, worldPoint)
+
+        element_bowling_ball.removeEventListener("collide", this.throwBowlingBall())
+
+        var scene = document.querySelector("#scene")
+        scene.removeChild(element_bowling_ball)
     }
 
 })
